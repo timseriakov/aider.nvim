4d87a047cb1fc0d054d6d2c16d1db109fa606012
Your Name <you@example.com>
2024-12-19 16:40:58 -0800

    fix: update models with script

--- gen-models.sh
+++ gen-models.sh
@@ -4,6 +4,14 @@ target_file=./lua/telescope/_extensions/model_data.lua
 
 echo 'return {' >$target_file
 
-aider --models e | grep '^- ' | sed -e 's/^- /  "/' -e 's/$/",/' | grep -v '^  "",$' >>$target_file
+get_models() {
+  search_pattern="$1"
+  aider --models "$search_pattern" |
+    grep '^- ' |
+    sed -e 's/^- /  "/' -e 's/$/",/' |
+    grep -v '^  "",$' >>$target_file
+}
+
+echo "$(get_models e)$(get_models i)" | sort | uniq >>$target_file
 
 echo '}' >>$target_file
