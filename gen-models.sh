7f875441f0a19ff254fd04a969234a401c3da626
Aaron Weisberg <aaronweisberg@gmail.com>
2024-12-19 16:25:58 -0800

    fix: Filter out empty entries from model list generation

--- gen-models.sh
+++ gen-models.sh
@@ -5,6 +5,6 @@ target_file=./lua/telescope/_extensions/model_data.lua
 echo 'return {' >$target_file
 
 # this is outputting some empty "" entries, can you filter those out ai!
-aider --models e | grep '^- ' | sed -e 's/^- /  "/' -e 's/$/",/' >>$target_file
+aider --models e | grep '^- ' | sed -e 's/^- /  "/' -e 's/$/",/' | grep -v '^  "",$' >>$target_file
 
 echo '}' >>$target_file
