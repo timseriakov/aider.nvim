49aa2a2b2b7a1bc6fefd1f199810d297ac2597ff
Your Name <you@example.com>
2024-12-19 16:25:57 -0800

    feat: Add script to generate models from OpenAPI spec

---
+++ gen-models.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+target_file=./lua/telescope/_extensions/model_data.lua
+
+echo 'return {' >$target_file
+
+# this is outputting some empty "" entries, can you filter those out ai!
+aider --models e | grep '^- ' | sed -e 's/^- /  "/' -e 's/$/",/' >>$target_file
+
+echo '}' >>$target_file
