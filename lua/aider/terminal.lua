51015055a4c568b2580fa15f4f633d7d7ec85894
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-14 20:06:53 +0300

    feat(terminal): add safe check for use_tmux configuration

--- lua/aider/terminal.lua
+++ lua/aider/terminal.lua
@@ -1,5 +1,29 @@
 local config = require("aider").config
-if vim.env.TMUX and config.use_tmux then
+
+-- безопасная проверка use_tmux
+if vim.env.TMUX and (config.use_tmux == true) then
 	return require("aider.tmux")
 end
-return require("aider.toggleterm")
+
+---@class AiderTerminal: table
+local term_mod = require("aider.toggleterm")
+
+-- оборачиваем on_term_open
+---@class AiderTerminal: table
+local term_mod = require("aider.toggleterm")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*toggleterm#*",
+	callback = function(args)
+		vim.schedule(function()
+			vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], {
+				buffer = args.buf,
+				desc = "Exit terminal to Normal mode (double ESC)",
+				noremap = true,
+				silent = true,
+			})
+		end)
+	end,
+})
+
+return term_mod
